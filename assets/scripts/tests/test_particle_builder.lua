bb64d92e37a9bceec8db3d2dfdbe617ea08aceb2
urgood2 <urgood2@gmail.com>
2025-12-16 22:12:23 +0900

    test: add Lua test infrastructure for particle builder

---
+++ assets/scripts/tests/test_particle_builder.lua
@@ -0,0 +1,9 @@
+-- assets/scripts/tests/test_particle_builder.lua
+local TestRunner = require("tests.test_runner")
+local ParticleMock = require("tests.mocks.particle_mock")
+
+-- Tests will be added here as we implement each feature
+
+return function()
+    TestRunner.run_all()
+end
